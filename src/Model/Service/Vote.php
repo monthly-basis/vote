dfe8d06943e71cee5c5bf1341a4be482cbf0dc6d
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-30 01:42:36 -0700

    Write vote service

--- src/Model/Service/Vote.php
+++ src/Model/Service/Vote.php
@@ -1,6 +1,8 @@
 <?php
 namespace LeoGalleguillos\Vote\Model\Service;
 
+use LeoGalleguillos\Entity\Model\Entity as EntityEntity;
+use LeoGalleguillos\User\Model\Entity as UserEntity;
 use LeoGalleguillos\Vote\Model\Table as VoteTable;
 
 class Vote
@@ -9,5 +11,21 @@ class Vote
         VoteTable\Vote $voteTable
     ) {
         $this->voteTable = $voteTable;
+    }
+
+    public function vote(
+        UserEntity\User $userEntity,
+        EntityEntity\Entity $entityEntity = null,
+        EntityEntity\EntityType $entityTypeEntity,
+        int $typeId,
+        int $value
+    ) {
+        $this->voteTable->insertOnDuplicateKeyUpdate(
+            $userEntity->getUserId(),
+            null,
+            $entityTypeEntity->getEntityTypeId(),
+            $typeId,
+            $value
+        );
     }
 }
