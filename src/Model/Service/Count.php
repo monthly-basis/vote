bab9c412322bea9e163f7ea439db2bd6adb20a2f
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-31 23:00:08 -0700

    Write service to get count of number of votes

---
+++ src/Model/Service/Count.php
@@ -0,0 +1,27 @@
+<?php
+namespace LeoGalleguillos\Vote\Model\Service;
+
+use LeoGalleguillos\Entity\Model\Entity as EntityEntity;
+use LeoGalleguillos\User\Model\Entity as UserEntity;
+use LeoGalleguillos\Vote\Model\Table as VoteTable;
+
+class Count
+{
+    public function __construct(
+        VoteTable\Vote $voteTable
+    ) {
+        $this->voteTable = $voteTable;
+    }
+
+    public function getCount(
+        EntityEntity\EntityType $entityTypeEntity,
+        int $typeId,
+        int $value
+    ) : int {
+        return $this->voteTable->selectWhereEntityTypeIdTypeIdValue(
+            $entityTypeEntity->getEntityTypeId(),
+            $typeId,
+            $value
+        );
+    }
+}
