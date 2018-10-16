9824abfd6587f6baa04048b1b5db57f6d28aa940
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-16 02:06:46 +0000

    Make up_votes and down_votes columns plural

--- sql/leogalle_test/vote_by_ip_total/create.sql
+++ sql/leogalle_test/vote_by_ip_total/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `vote_by_ip_total` (
     `entity_type_id` int(10) unsigned not null,
     `type_id` int(10) unsigned not null,
-    `up_vote` int(1) unsigned not null default '0',
-    `down_vote` int(1) unsigned not null default '0',
+    `up_votes` int(1) unsigned not null default '0',
+    `down_votes` int(1) unsigned not null default '0',
     PRIMARY KEY (`entity_type_id`, `type_id`)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
