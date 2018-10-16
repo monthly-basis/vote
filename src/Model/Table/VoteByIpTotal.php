a8a2fc73764acad2eaf21ea3d567285a626e26e1
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-16 02:06:13 +0000

    Write table model for vote_by_ip_total table

---
+++ src/Model/Table/VoteByIpTotal.php
@@ -0,0 +1,63 @@
+<?php
+namespace LeoGalleguillos\Vote\Model\Table;
+
+use Exception;
+use Generator;
+use TypeError;
+use Zend\Db\Adapter\Adapter;
+
+class VoteByIpTotal
+{
+    /**
+     * @var Adapter
+     */
+    protected $adapter;
+
+    public function __construct(Adapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    public function incrementUpVotes(
+        int $entityTypeId,
+        int $typeId
+    ):int {
+        $sql = '
+            UPDATE `vote_by_ip_total`
+               SET `up_votes` = `up_votes` + 1
+             WHERE `entity_type_id` = ?
+               AND `type_id` = ?
+        ';
+        $parameters = [
+            $entityTypeId,
+            $typeId,
+        ];
+        return (int) $this->adapter
+                          ->query($sql)
+                          ->execute($parameters)
+                          ->getAffectedRows();
+    }
+
+    public function insertIgnore(
+        int $entityTypeId,
+        int $typeId
+    ): int {
+        $sql = '
+            INSERT IGNORE
+              INTO `vote_by_ip_total` (
+                     , `entity_type_id`
+                     , `type_id`
+                   )
+            VALUES (?, ?)
+                 ;
+        ';
+        $parameters = [
+            $entityTypeId,
+            $typeId,
+        ];
+        return (int) $this->adapter
+                          ->query($sql)
+                          ->execute($parameters)
+                          ->getAffectedRows();
+    }
+}
