d20019b00517e05513cb5f656ca76ab37be060d3
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-16 01:10:09 +0000

    Fix primary key

--- sql/leogalle_test/vote_by_ip/create.sql
+++ sql/leogalle_test/vote_by_ip/create.sql
@@ -3,5 +3,5 @@ CREATE TABLE `vote_by_ip` (
     `entity_type_id` int(10) unsigned not null,
     `type_id` int(10) unsigned not null,
     `value` int(1) signed not null,
-    PRIMARY KEY (`ip`, `entity_type_id`)
+    PRIMARY KEY (`ip`, `entity_type_id`, `type_id`)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
