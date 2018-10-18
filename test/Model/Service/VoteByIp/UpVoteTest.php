e29c128d8e8919338ba0a620cd3960a8e505e792
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-18 21:00:02 +0000

    Write preliminary upvote test

---
+++ test/Model/Service/VoteByIp/UpVoteTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace LeoGalleguillos\VoteTest\Model\Service\VoteByIp;
+
+use Exception;
+use LeoGalleguillos\Entity\Model\Entity as EntityEntity;
+use LeoGalleguillos\User\Model\Entity as UserEntity;
+use LeoGalleguillos\Vote\Model\Entity as VoteEntity;
+use LeoGalleguillos\Vote\Model\Factory as VoteFactory;
+use LeoGalleguillos\Vote\Model\Service as VoteService;
+use LeoGalleguillos\Vote\Model\Table as VoteTable;
+use PHPUnit\Framework\TestCase;
+use Zend\Db\Adapter\Driver\Pdo\Connection;
+
+class UpVoteTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->connectionMock = $this->createMock(
+            Connection::class
+        );
+        $this->voteByIpTableMock = $this->createMock(
+            VoteTable\VoteByIp::class
+        );
+        $this->votesTableMock = $this->createMock(
+            VoteTable\Votes::class
+        );
+        $this->upVoteService = new VoteService\VoteByIp\UpVote(
+            $this->connectionMock,
+            $this->voteByIpTableMock,
+            $this->votesTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            VoteService\VoteByIp\UpVote::class,
+            $this->upVoteService
+        );
+    }
+}
